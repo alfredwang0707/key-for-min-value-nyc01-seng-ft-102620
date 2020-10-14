890411016dc890195f5fb3e9b3c548898d7223ed
Alfred Wang <alfredwang0707@gmail.com>
2020-10-14 23:49:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,7 @@ name_hash.each do |key, value|
   #iterate and find a lower one, if its lower, replace the #current one
   #If the method is called and passed an argument of an #empty hash, it should return nil
   if lowest_value == nil || value < lowest_value
-    lowest value == value
-    lowest key == key
+    lowest_value = value
+    lowest_key = key
+  end
 end
